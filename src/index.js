9f77ec11cece7b410e64e070a8aee2671c2d86de
Jasmine Chouhan <jasminechouhan091@gmail.com>
2025-03-16 19:34:45 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./env"
+})
+
+// import express from "express";
+// const app = express();
+
+// function connectDB() {}
+// connectDB()
+
+
+connectDB()
+
+
+
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`app is listening on port: , ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw err
+        
+    }
+})()
+    */
